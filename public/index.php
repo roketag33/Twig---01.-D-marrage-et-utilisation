a8eb2c0749431e4f17be998fe7905f80cdd4ecbf
alexPro <alex@PRT-BB-ASN2302>
2023-02-24 11:13:48 +0100

    exo done

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@
 require_once __DIR__ . '/../config/twig.php';
 
 
-$name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
+
+
+
